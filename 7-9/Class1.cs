1f04206011184b6b5934fa6b3c03189d1d772c7c
Roma <romap8654@gmail.com>
2023-11-10 17:51:21 +0300

    On

---
+++ 7-9/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_9
+{
+    public class Class1
+    {
+        public string Name { get; set; }
+        public Class1(string name)
+        {
+        Name = name;
+
+        }
+
+        public void Print(string name)
+        {
+            Console.WriteLine(Name.Length);
+        }
+    }
+}
---
+++ 7-9/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1(Console.ReadLine());
+            class1.Print(" ");
+            Console.ReadLine();
+        }
+    }
+}
